6ee195a87da3d33b14745677d6c0561e51479041
George Fourtounis <gfour@di.uoa.gr>
2019-12-12 17:54:45 +0200

    Add tests.

---
+++ src/test/groovy/org/clyze/persistent/TestClass.groovy
@@ -0,0 +1,12 @@
+package org.clyze.persistent
+
+import groovy.transform.EqualsAndHashCode
+
+/**
+ * Created by saiko on 10/11/2015.
+ */
+@EqualsAndHashCode
+class TestClass {
+    String memberStr
+    Integer memberInt
+}
